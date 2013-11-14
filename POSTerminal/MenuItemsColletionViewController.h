2d0a2f190d1e0b7c7721330dcaf97bab44ed3a61
Jonathan <jonathanrfisher@yahoo.com>
2013-11-14 10:05:07 -0500

    Committing to my branch before trying to bring up Luam’s and merge our branches

---
+++ POSTerminal/MenuItemsColletionViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuItemsColletionViewController.h
+//  POSTerminal
+//
+//  Created by Katherine Fisher on 11/12/13.
+//  Copyright (c) 2013 Jonathan Fisher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuItemsColletionViewController : UICollectionView
+
+@end
