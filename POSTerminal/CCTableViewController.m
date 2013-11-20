3eb6054b0bf455ed2a77ecd8d1ebdbe122e93352
Luam <luam13@gmail.com>
2013-11-20 04:57:41 -0500

    CC Table view controller

--- POSTerminal/CCTableViewController.m
+++ POSTerminal/CCTableViewController.m
@@ -88,6 +88,21 @@
     return 0;
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    //#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    // Configure the cell...
+    
+    return 0;
+}
+
 /*
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
